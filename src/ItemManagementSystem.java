955e45c4d3d79b67f79e4edf62e3291ad8021ce0
Josue RUSUMBA <rusumbaamanijosue@gmail.com>
2024-10-29 18:50:49 +0300

    Commit #20
    I have put a void method and also store them in an array of the abstract type then i add a loop through the collection to demonstrate polymorphism

--- src/ItemManagementSystem.java
+++ src/ItemManagementSystem.java
@@ -1,10 +1,17 @@
 public class ItemManagementSystem {
+    public static void main(String[] args) {
 
-    Item book = new Book("The Great Gatsby", "Joshua RUSUMBA", "9780743273565");
-    Item laptop = new Electronic("Laptop", "hp", "intel core i7 vPro 8th Gen", true);
-    Item chair = new Fourniture("Chair", "Wood", 200);
-    Item phone = new Electronic("Phone", "Apple", "iPhone 14", false);
+        Item book = new Book("The Great Gatsby", "Joshua RUSUMBA", "9780743273565");
+        Item laptop = new Electronic("Laptop", "hp", "intel core i7 vPro 8th Gen", true);
+        Item chair = new Fourniture("Chair", "Wood", 200);
+        Item phone = new Electronic("Phone", "Apple", "iPhone 14", false);
 
+        Item[] items = {book, laptop, chair, phone};
 
-
+        for (Item item : items) {
+            item.showDetails();
+            item.performAction();
+            System.out.println();
+        }
+    }
 }
