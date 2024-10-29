8e9974cbae4670e3127de828b3bc8d0636a9eecc
Josue RUSUMBA <rusumbaamanijosue@gmail.com>
2024-10-29 18:11:57 +0300

    Commit #13
    This constructor will help us to see what is in our package which i made private

--- src/Electronic.java
+++ src/Electronic.java
@@ -3,4 +3,11 @@ public class Electronic {
     private String brand;
     private String model;
     private boolean isPoweredOn;
+
+    public Electronic(String name, String brand, String model, boolean isPoweredOn) {
+        super(name);
+        this.brand = brand;
+        this.model = model;
+        this.isPoweredOn = isPoweredOn;
+    }
 }
