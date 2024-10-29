cc7679468d4e4ad8dd58424e2cdb14e1f4db865a
Josue RUSUMBA <rusumbaamanijosue@gmail.com>
2024-10-29 17:55:25 +0300

    Commit #9
    I Overriding showDetails for Fourniture-specific details

--- src/Fourniture.java
+++ src/Fourniture.java
@@ -14,4 +14,12 @@ public class Fourniture {
         System.out.println("Performing action: Checking stability...");
         System.out.println("Action Result: The " + material + " furniture can hold up to " + weightCapacity + "kg.");
     }
+
+    @Override
+    public void showDetails() {
+        System.out.println("Type: Furniture");
+        super.showDetails();
+        System.out.println("Material: " + material);
+        System.out.println("Weight Capacity: " + weightCapacity + "kg");
+    }
 }
