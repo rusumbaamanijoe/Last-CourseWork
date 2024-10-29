f4995c1ebf7ec0cc930775106c6a1b412b8fb74f
Josue RUSUMBA <rusumbaamanijosue@gmail.com>
2024-10-29 17:50:44 +0300

    Commit #8
    I have put the contructor of variable that i have declared

--- src/Fourniture.java
+++ src/Fourniture.java
@@ -3,4 +3,10 @@ public class Fourniture {
     private String material;
     private int weightCapacity;
 
+    public Fourniture(String name, String material, int weightCapacity) {
+        super(name);
+        this.material = material;
+        this.weightCapacity = weightCapacity;
+    }
+
 }
