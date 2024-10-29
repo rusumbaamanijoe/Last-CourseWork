38bbf8a875dc26ff1c0660bb13dc85ef2e2f9566
Josue RUSUMBA <rusumbaamanijosue@gmail.com>
2024-10-29 18:15:29 +0300

    Commit #14
    I implementing the abstract method for Electronic-specific behavior

--- src/Electronic.java
+++ src/Electronic.java
@@ -10,4 +10,16 @@ public class Electronic {
         this.model = model;
         this.isPoweredOn = isPoweredOn;
     }
+
+    @Override
+    public void performAction() {
+        if (isPoweredOn) {
+            System.out.println("Performing action: Running diagnostics...");
+            System.out.println("Action Result: Diagnostics completed. No issues found in " + brand + " " + model + ".");
+        } else {
+            System.out.println("Performing action: Powering on...");
+            isPoweredOn = true;
+            System.out.println("Action Result: The " + model + " is now powered on.");
+        }
+    }
 }
