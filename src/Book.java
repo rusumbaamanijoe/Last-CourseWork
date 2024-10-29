57802ced9bcdefe9f298dad0f4a158e881c27ed7
Josue RUSUMBA <rusumbaamanijosue@gmail.com>
2024-10-29 17:34:11 +0300

    Commit #3
    i have declared my variables as private then i also put its constructor

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,10 @@
 public class Book {
+    private String author;
+    private String isbn;
+
+    public Book(String name, String author, String isbn) {
+        super(name);
+        this.author = author;
+        this.isbn = isbn;
+    }
 }
